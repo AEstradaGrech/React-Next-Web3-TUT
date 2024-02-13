0cd2b826918a7cb3874db86664eabb8e05b9a2a3
Alvaro Estrada <alvaro.estrada89@gmail.com>
2024-02-13 23:19:27 +0100

    [contract-deploy] --> improved deploy & compile scripts

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,8 +1,13 @@
 const solc = require('solc');
-const fs = require('fs');
-const path = require('path');
+const Web3 = require('web3');
+const fs = require('fs-extra');
+const path = require('path')
+
+const buildPath = path.resolve(__dirname, 'builds');
+fs.removeSync(buildPath);
+
 const contractPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
-const source = fs.readFileSync(contractPath, 'utf8');
+const source = fs.readFileSync(contractPath, 'utf-8');
 const input = {
     language: 'Solidity',
     sources: {
@@ -18,18 +23,18 @@ const input = {
       },
     },
   };
-  console.log(JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'].CampaignFactory);
-  module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'].CampaignFactory;
-// const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'];
 
-// fs.ensureDirSync(buildPath);
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'];
 
-// for (let contract in output)
-// {
-//    fs.outputJSONSync(path.resolve(buildPath, contract + '.json'), output[contract]);
-// }
+fs.ensureDirSync(buildPath);
+const version = '0.0.1';
+for (let contract in output)
+{
+   fs.outputJSONSync(path.resolve(buildPath, `${contract}-v${version}.json`), output[contract]);
+}
 
 // console.log(':: ABI :: \n',output['CampaignFactory'].abi);
 // console.log(':: EVM :: \n',output['CampaignFactory'].evm);
 //module.exports = output['CampaignFactory'];
 //console.log(':: MODULE EXPORTS :: \n', JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'].CampaignFactory);
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'].CampaignFactory;
--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,10 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-const { abi, evm } = require('./compile');
+const { abi, evm } = require('./builds/CampaignFactory-v0.0.1.json');
+const path = require('path');
+const fs = require('fs-extra');
+
+const buildsPath = path.resolve(__dirname, 'builds');
 
 const pnemonic = 'access action trip ice inflict invite assume maze primary student travel flock';
 const endpoint = 'https://sepolia.infura.io/v3/22814a84a4ab473bb5586697bea1e5b5';
@@ -8,7 +12,7 @@ const endpoint = 'https://sepolia.infura.io/v3/22814a84a4ab473bb5586697bea1e5b5'
 const provider = new HDWalletProvider( pnemonic, endpoint);
 
 const web3 = new Web3(provider);
-
+const version = '0.0.1';
 const deploy = async () => {
     let accounts = await web3.eth.getAccounts();
     console.log('Deploying contract from address: ', accounts[0]);
@@ -16,6 +20,7 @@ const deploy = async () => {
         .deploy({data: evm.bytecode.object})
         .send({from: accounts[0], gas: '2000000'});
     console.log(JSON.stringify(abi));
+    fs.outputJSONSync(path.resolve(__dirname,'builds', `campaignsFactory-v${version}-deploy.json`), { creator: accounts[0] , address: contract.options.address, abi: abi });
     console.log('Contract deployed to address: ', contract.options.address );
     provider.engine.stop();
 }
@@ -23,5 +28,7 @@ const deploy = async () => {
 
 deploy()
 
-// 13/02/24 -> Factory Address: 0x7BC1F6334c397CA12831498810c165123A44da7A
+// 13/02/24 -> Factory Address: 0x7BC1F6334c397CA12831498810c165123A44da7A, 
+//                              0x9eC5dc4ca57B4C21e176d85a33E4C013489897dd (dsde json)
 // 13/02/24 -> Factory Abi: [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"minAmount","type":"uint256"}],"name":"createCampaing","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getCreatorAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getDeployedCampaigns","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"}] 
+//              (dsd json): [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"minAmount","type":"uint256"}],"name":"createCampaing","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getCreatorAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getDeployedCampaigns","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"}]
