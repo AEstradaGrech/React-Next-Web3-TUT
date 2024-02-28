e47b68c541dd3b046f0de6e8ee1169fda29a4299
Alvaro Estrada <alvaro.estrada89@gmail.com>
2024-02-28 23:12:21 +0100

    [campaign-detail] --> adding cards for campaign summary props

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import web3 from '../../ethereum/web3.js'
 import Campaign from '../../ethereum/campaign.js'
 import Layout from '../../components/layout';
@@ -17,14 +18,57 @@ class CampaignShowComponent extends Component{
             title: summary.description, 
             minContribution: web3.utils.fromWei(summary.minContribution, 'ether'),
             totalContribution: web3.utils.fromWei(summary.balance, 'ether'),
-            contributors: summary.contributors
+            contributors: parseInt(summary.contributors) 
         }
     }
 
+    renderCards(){
+        const {
+            managerAddress,
+            title,
+            minContribution,
+            totalContribution,
+            contributors
+        } = this.props;
+
+        const items = [
+            {
+                header: managerAddress,
+                meta: 'Address of campaign creator',
+                description: 'The wallet address that deployed the campaign',
+                style: { overflowWrap: 'break-word'}
+            },
+            {
+                header: title,
+                meta: 'Campaign description',
+                description: 'whatever the fuck the manager wants to pay with your money'
+            },
+            {
+                header: minContribution,
+                meta: 'Minimum contribution (ETH)',
+                description: 'The minimum amount you have to pay to become an approver'
+            },
+            {
+                header: totalContribution,
+                meta: 'Total contribution (ETH)',
+                description: 'The actual amount of ETH available for this campaign'
+            },
+            {
+                header: contributors,
+                meta: 'Total contributors',
+                description: 'The actual number of contributors who have joined this campaign '
+            }
+        ]
+
+        return <Card.Group items={items}/>
+  
+        
+    }
     render(){
         return(
             <Layout>
                 <h3>{this.props.title}</h3>
+                {this.renderCards()} 
             </Layout>
         )
     }
