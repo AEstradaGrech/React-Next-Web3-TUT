2bcf325f12db94fe6cb7fefe269a75c2ee534bb7
Alvaro Estrada <alvaro.estrada89@gmail.com>
2024-03-04 20:45:24 +0100

    [requests-index] --> routing OK. mocking req cards.

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react'
+import Campaign from '../../../ethereum/campaign.js'
+import Layout from '../../../components/layout.js';
+class RequestsIndex extends Component {
 
-class RequestsIndexComponent extends Component {
-    static async getInitialProps(){
-        return { }
+    static async getInitialProps(props)
+    {
+        const campaign = Campaign(props.query.address);
+
+        const summary = await campaign.methods.getSummary().call();
+
+        console.log('CAMPAIGN SUMMARY:',summary);
+        return { address: props.query.address }
+    }
+
+    renderCards(){
+        const items = [
+            {
+                header:'Request 1',
+                description: 'ReqDesc',
+                fluid: true
+            },
+            {
+                header: 'Request 2',
+                description: 'Req2Desc',
+                fluid:true
+            }
+        ]
+
+        return <Card.Group items={items}/>
     }
     render(){
         return(
-            <h1>Requests Index Page</h1>
+            <Layout>
+                <h3>Active Requests</h3>
+                {this.renderCards()}
+            </Layout>
+            
         )
     }
 }
 
-export default RequestsIndexComponent;
+export default RequestsIndex;
